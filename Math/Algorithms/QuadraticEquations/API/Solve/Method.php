73ea33331e39f458bb1378e8c5f60b08874efc90
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-06-07 23:08:06 +0300

    [0.1.4] Add error check.

--- Math/Algorithms/QuadraticEquations/API/Solve/Method.php
+++ Math/Algorithms/QuadraticEquations/API/Solve/Method.php
@@ -6,7 +6,6 @@ use Liloi\API\Response;
 use Liloi\Mini\Math\Algorithms\QuadraticEquations\API\Method as SuperMethod;
 use Liloi\MathAlg\Simple\QuadraticEquations\Algorithm;
 use Liloi\MathAlg\Simple\QuadraticEquations\Solution;
-use Liloi\MathAlg\Simple\QuadraticEquations\Exception;
 
 /**
  * Rune API: Blueprint.Blueprints.Show
@@ -16,23 +15,37 @@ class Method extends SuperMethod
 {
     public static function execute(): Response
     {
-        $solution = Algorithm::solve(
-            self::getParameter('a'),
-            self::getParameter('b'),
-            self::getParameter('c')
-        );
-
         $response = new Response();
 
-        if($solution->areNoRoots())
-        {
-            $response->set('render', static::render(__DIR__ . '/NoRoots.tpl'));
+        try {
+            $solution = Algorithm::solve(
+                self::getParameter('a'),
+                self::getParameter('b'),
+                self::getParameter('c')
+            );
+
+            if($solution->areNoRoots())
+            {
+                $response->set('render', static::render(__DIR__ . '/NoRoots.tpl'));
+            }
+            else
+            {
+                $response->set('render', static::render(__DIR__ . '/Template.tpl', [
+                    'x1' => $solution->getFirstRoot(),
+                    'x2' => $solution->getSecondRoot()
+                ]));
+            }
         }
-        else
+        catch (\TypeError)
         {
-            $response->set('render', static::render(__DIR__ . '/Template.tpl', [
-                'x1' => $solution->getFirstRoot(),
-                'x2' => $solution->getSecondRoot()
+            $response->set('render', static::render(__DIR__ . '/Error.tpl', [
+                'message' => 'Argument(s) must be of type float, string given; correct, please.'
+            ]));
+        }
+        catch (\Error|\Exception $e)
+        {
+            $response->set('render', static::render(__DIR__ . '/Error.tpl', [
+                'message' => $e->getMessage()
             ]));
         }
 
